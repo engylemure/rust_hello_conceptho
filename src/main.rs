838dd7137f34477fac7de2ec8e9f5c8901020e44
Jordão Rodrigues Oliveira Rosario <jordao.rosario01@gmail.com>
2020-01-15 01:40:43 -0300

    A web with shared state guessing game implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 use std::sync::Mutex;
+
+use actix_web::{middleware, web, App, HttpResponse, HttpServer};
 
 struct GuessGame {
     pub secret: u32,
     pub number_of_tries: Mutex<u32>,
-    pub tries: Vec<Mutex<bool>>
+    pub tries: Vec<Mutex<bool>>,
 }
 
 enum PlayResult {
     AlreadyTried,
-    Value(Ordering)
+    Value(Ordering),
 }
 
 impl GuessGame {
@@ -23,7 +25,7 @@ impl GuessGame {
         GuessGame {
             secret: rand::thread_rng().gen_range(1, 101),
             number_of_tries: Mutex::new(0),
-            tries 
+            tries,
         }
     }
 
@@ -39,45 +41,52 @@ impl GuessGame {
                     PlayResult::Value(guess.cmp(&self.secret))
                 }
             }),
-            None => None
-        } 
+            None => None,
+        }
     }
 }
 
-fn main() {
-   println!("Guess the Number!");
-
-   let  mut game = GuessGame::new();
-
-   println!("The secret number is: {}", game.secret);
-
-   loop {
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
- 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
- 
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue
+async fn index(shared_state: web::Data<Mutex<GuessGame>>, path: web::Path<u32>) -> HttpResponse {
+    let result = shared_state.lock().unwrap().play(path.into_inner());
+    let number_of_tries: u32 = *shared_state.lock().unwrap().number_of_tries.lock().unwrap();
+    let result = {
+        match result {
+            Some(result) => match result {
+                PlayResult::AlreadyTried => "This value was already used!",
+                PlayResult::Value(value) => match value {
+                    Ordering::Less => "Too small!",
+                    Ordering::Greater => "Too big!",
+                    Ordering::Equal => {
+                        *shared_state.lock().unwrap() = GuessGame::new();
+                        "You win!"
+                    }
+                },
+            },
+            None => "The value should be between 1 and 100!",
+        }
     };
- 
-    println!("You guessed: {}", guess);
-    
-    match game.play(guess) {
-        Some(result) => match result {
-            PlayResult::AlreadyTried => println!("This value was already used!"),
-            PlayResult::Value(value) => match value {
-                Ordering::Less => println!("Too small!"),
-                Ordering::Greater => println!("Too big!"),
-                Ordering::Equal =>  {
-                    println!("You win!");
-                    break;
-                }
-            }
-        },
-        None => println!("The value should be between 1 and 100!")
-    }
-   }
+    let body = format!(
+        "{{\"number_of_tries\": {}, \"result\": \"{}\" }}\n",
+        number_of_tries, result
+    );
+    HttpResponse::Ok().body(body)
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    println!("Guess the Number!");
+    let shared_state = web::Data::new(Mutex::new(GuessGame::new()));
+    HttpServer::new(move || {
+        App::new()
+            .app_data(shared_state.clone())
+            // enable logger
+            .wrap(middleware::Logger::default())
+            // register simple handler
+            .service(web::resource("/{guess}").to(index))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
